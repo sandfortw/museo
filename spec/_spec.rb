53ef8f2f157bfe1990c1bf0d61b0044d1ee337b1
Weston Sandfort <sandfortw@gmail.com>
2023-01-12 17:52:01 -0700

    iteration 3 tests

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -212,15 +212,213 @@
   describe "iteration 3" do
 
     it '#photographs by artist' do 
-
+      curator = Curator.new # => #<Curator:0x00007fabc6c2b680...>
+      
+      photo_1 = Photograph.new({
+          id: "1",      
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+          artist_id: "1",      
+          year: "1954"      
+     }) # => #<Photograph:0x00007fabc6933180...>
+     
+      photo_2 = Photograph.new({    
+          id: "2",      
+          name: "Moonrise, Hernandez",      
+          artist_id: "2",      
+          year: "1941"      
+     }) # => #<Photograph:0x00007fabc6c28e58...>
+     
+      photo_3 = Photograph.new({    
+          id: "3",      
+          name: "Identical Twins, Roselle, New Jersey",      
+          artist_id: "3",      
+          year: "1967"      
+     }) # => #<Photograph:0x00007fabc5bb9ef0...>
+     
+      photo_4 = Photograph.new({    
+          id: "4",      
+          name: "Monolith, The Face of Half Dome",      
+          artist_id: "3",      
+          year: "1927"      
+     })  # => #<Photograph:0x00007fabc6b931f0...>
+     
+      artist_1 = Artist.new({    
+          id: "1",      
+          name: "Henri Cartier-Bresson",      
+          born: "1908",      
+          died: "2004",      
+          country: "France"      
+     })  # => #<Artist:0x00007fabc6a52340...>
+     
+      artist_2 = Artist.new({    
+          id: "2",      
+          name: "Ansel Adams",      
+          born: "1902",      
+          died: "1984",      
+          country: "United States"      
+     })  # => #<Artist:0x00007fabc6c20870...>
+     
+      artist_3 = Artist.new({    
+          id: "3",      
+          name: "Diane Arbus",      
+          born: "1923",      
+          died: "1971",      
+          country: "United States"      
+     })  # => #<Artist:0x00007fabc5ba0c70...>
+     
+      curator.add_artist(artist_1)  
+      curator.add_artist(artist_2)    
+      curator.add_artist(artist_3)
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      curator.add_photograph(photo_3)
+      curator.add_photograph(photo_4)
+      expect(curator.photographs_by_artist).to eq({
+        artist_1 => [photo_1],
+        artist_2 => [photo_2],
+        artist_3 => [photo_3, photo_4]
+      })
+     # => {
+     #        #<Artist:0x00007fabc6a52340...> => [#<Photograph:0x00007fabc6933180...>],
+     #        #<Artist:0x00007fabc6c20870...> => [#<Photograph:0x00007fabc6c28e58...>],
+     #        #<Artist:0x00007fabc5ba0c70...> => [#<Photograph:0x00007fabc5bb9ef0...>, #<Photograph:0x00007fabc6b931f0...>]
+     #      }
     end
 
     it '#artists_with_multiple_photographs' do 
-
+      curator = Curator.new # => #<Curator:0x00007fabc6c2b680...>
+      
+      photo_1 = Photograph.new({
+          id: "1",      
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+          artist_id: "1",      
+          year: "1954"      
+     }) # => #<Photograph:0x00007fabc6933180...>
+     
+      photo_2 = Photograph.new({    
+          id: "2",      
+          name: "Moonrise, Hernandez",      
+          artist_id: "2",      
+          year: "1941"      
+     }) # => #<Photograph:0x00007fabc6c28e58...>
+     
+      photo_3 = Photograph.new({    
+          id: "3",      
+          name: "Identical Twins, Roselle, New Jersey",      
+          artist_id: "3",      
+          year: "1967"      
+     }) # => #<Photograph:0x00007fabc5bb9ef0...>
+     
+      photo_4 = Photograph.new({    
+          id: "4",      
+          name: "Monolith, The Face of Half Dome",      
+          artist_id: "3",      
+          year: "1927"      
+     })  # => #<Photograph:0x00007fabc6b931f0...>
+     
+      artist_1 = Artist.new({    
+          id: "1",      
+          name: "Henri Cartier-Bresson",      
+          born: "1908",      
+          died: "2004",      
+          country: "France"      
+     })  # => #<Artist:0x00007fabc6a52340...>
+     
+      artist_2 = Artist.new({    
+          id: "2",      
+          name: "Ansel Adams",      
+          born: "1902",      
+          died: "1984",      
+          country: "United States"      
+     })  # => #<Artist:0x00007fabc6c20870...>
+     
+      artist_3 = Artist.new({    
+          id: "3",      
+          name: "Diane Arbus",      
+          born: "1923",      
+          died: "1971",      
+          country: "United States"      
+     })  # => #<Artist:0x00007fabc5ba0c70...>
+     
+      curator.add_artist(artist_1)  
+      curator.add_artist(artist_2)    
+      curator.add_artist(artist_3)
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      curator.add_photograph(photo_3)
+      curator.add_photograph(photo_4)
+      expect(curator.artists_with_multiple_photographs).to eq(["Diane Arbus"])
+     
     end
 
     it '#photographs_taken_by_artist_from(country)' do
+       curator = Curator.new # => #<Curator:0x00007fabc6c2b680...>
+      
+       photo_1 = Photograph.new({
+           id: "1",      
+           name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+           artist_id: "1",      
+           year: "1954"      
+      }) # => #<Photograph:0x00007fabc6933180...>
+      
+       photo_2 = Photograph.new({    
+           id: "2",      
+           name: "Moonrise, Hernandez",      
+           artist_id: "2",      
+           year: "1941"      
+      }) # => #<Photograph:0x00007fabc6c28e58...>
+      
+       photo_3 = Photograph.new({    
+           id: "3",      
+           name: "Identical Twins, Roselle, New Jersey",      
+           artist_id: "3",      
+           year: "1967"      
+      }) # => #<Photograph:0x00007fabc5bb9ef0...>
+      
+       photo_4 = Photograph.new({    
+           id: "4",      
+           name: "Monolith, The Face of Half Dome",      
+           artist_id: "3",      
+           year: "1927"      
+      })  # => #<Photograph:0x00007fabc6b931f0...>
+      
+       artist_1 = Artist.new({    
+           id: "1",      
+           name: "Henri Cartier-Bresson",      
+           born: "1908",      
+           died: "2004",      
+           country: "France"      
+      })  # => #<Artist:0x00007fabc6a52340...>
+      
+       artist_2 = Artist.new({    
+           id: "2",      
+           name: "Ansel Adams",      
+           born: "1902",      
+           died: "1984",      
+           country: "United States"      
+      })  # => #<Artist:0x00007fabc6c20870...>
+      
+       artist_3 = Artist.new({    
+           id: "3",      
+           name: "Diane Arbus",      
+           born: "1923",      
+           died: "1971",      
+           country: "United States"      
+      })  # => #<Artist:0x00007fabc5ba0c70...>
+      
+       curator.add_artist(artist_1)  
+       curator.add_artist(artist_2)    
+       curator.add_artist(artist_3)
+       curator.add_photograph(photo_1)
+       curator.add_photograph(photo_2)
+       curator.add_photograph(photo_3)
+       curator.add_photograph(photo_4)
 
+       expect(curator.photographs_taken_by_artist_from("United States")).to eq([photo_2, photo_3, photo_4])
+      # => [#<Photograph:0x00007fabc6c28e58...>, #<Photograph:0x00007fabc5bb9ef0...>, #<Photograph:0x00007fabc6b931f0...>
+      
+       expect(curator.photographs_taken_by_artist_from("Argentina")).to eq([])
+      # => []
     end
   end
 
