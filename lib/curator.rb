0e031d981dd08c2d9b73d5df59722122b0c7d67c
Weston Sandfort <sandfortw@gmail.com>
2023-01-12 18:03:07 -0700

    Finish iteration 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -20,4 +20,37 @@ class Curator
       artist.id == id_string
     end
   end
+
+  def photographs_by_artist
+    hash = {}
+    @artists.each do |artist|
+      hash[artist] = []
+    end
+   
+    @artists.each do |artist|
+      @photographs.each do |photo|
+        hash[artist] << photo if photo.artist_id == artist.id
+      end
+    end
+    hash
+  end
+
+  def photographs_taken_by_artist_from(country)
+    array = []
+    @photographs.each do |photo|
+      @artists.each do |artist|
+        array << photo if photo.artist_id == artist.id && artist.country == country
+      end
+    end
+    array
+  end
+
+  def artists_with_multiple_photographs
+    array = []
+    photographs_by_artist.each do |artist, album|
+      array << artist.name if album.size > 1
+    end
+    array
+  end
+
 end
